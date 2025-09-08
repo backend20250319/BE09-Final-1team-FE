de2cf224908a10df2dc6f65ed3677875cdb9028a
parkchangjun <parkchangjun13@gmail.com>
2025-09-08 09:43:01 +0900

    [DELETE] : 인기 키워드 삭제

--- app/MainPage.jsx
+++ app/MainPage.jsx
@@ -411,7 +411,7 @@ export default function MainPage({
     <div className="min-h-screen bg-gradient-to-br from-blue-50 via-indigo-50 to-purple-50">
       <div className="max-w-7xl mx-auto px-4 py-10">
         <div className="lg:col-span-3">
-          <div className="mb-6">
+          <div>
             <div className="flex flex-col gap-4">
               <div className="w-full overflow-x-auto flex space-x-3 pb-2">
                 {categories.map((category, index) => (
@@ -432,7 +432,7 @@ export default function MainPage({
                 ))}
               </div>
               <div className="w-full">
-                <RealTimeKeywordWidget width="100%" />
+                {/*<RealTimeKeywordWidget width="100%" />*/}
               </div>
             </div>
           </div>
